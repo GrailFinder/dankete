1f3f72607a28f01efa044fbb0183ac974411c021
grail <wohilas@gmail.com>
2018-05-25 14:56:43 +0300

    nested serializers

--- ankete/api/serializers.py
+++ ankete/api/serializers.py
@@ -3,12 +3,37 @@ from django.contrib.auth.models import User
 from ankete.models import Inquiry, Question, Choice
 from rest_framework import serializers
 
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'date_joined', 'last_login']
+
+
+class QuestionSerializer(ModelSerializer):
+    #answers = PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        depth = 2
+        fields = [
+            'id',
+            'title',
+            'created_at',
+            'multiansw',
+            'answers',
+        ]
+
+
 class InquirySerializer(ModelSerializer):
-    questions = PrimaryKeyRelatedField(many=True, read_only=True)
-    owner = serializers.ReadOnlyField(source="owner.username")
+    #questions = PrimaryKeyRelatedField(many=True, read_only=True)
+    #owner = serializers.ReadOnlyField(source="owner.username")
+    owner = PrimaryKeyRelatedField(many=True, read_only=True)
+    questions = QuestionSerializer(many=True)
 
     class Meta:
         model = Inquiry
+        depth = 3
         fields = [
             'id',
             'title',
@@ -18,32 +43,18 @@ class InquirySerializer(ModelSerializer):
             'owner',
         ]
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username',
-        'email', 'date_joined', 'last_login']
-
-class QuestionSerializer(ModelSerializer):
-    answers = PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Question
-        fields = ['id',
-            'title',
-            'created_at',
-            'multiansw',
-            'answers',
-            ]
 
 class ChoiceSerializer(ModelSerializer):
     class Meta:
+        depth = 1
         model = Choice
-        fields = ['id',
-        'text',
-        'created_at',
-        'value',
+        fields = [
+            'id',
+            'text',
+            'created_at',
+            'value',
         ]
+
 
 class AnswerSerializer(ModelSerializer):
     class Meta:
--- ankete/settings.py
+++ ankete/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'q_dkx1uu!=mu^^&qk(qqjcdho1yiul*&i$ld$n*s7e=lv$v#x%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '104.131.170.212']
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ankete.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -88,25 +85,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -120,7 +119,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
